b2b7766862bdd9341db5244d40ee0eff172905fe
raudelf <rawdell@outlook.com>
2020-03-02 23:28:31 -0600

    Added in the Private Route and imported the Dashboard.js file

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Login from './components/Login';
 import SignUpForm from './components/SignUpForm';
+import PrivateRoute from './components/PrivateRoute';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Navigation />
       <h1>Spotify Song Suggester</h1>
       <Switch>
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={SignUpForm} />
       </Switch>
