a1aafc5da6deb6f38253a17dc5bff5db71ead66d
raudelf <rawdell@outlook.com>
2020-03-02 23:11:10 -0600

    created the base for the dashboard page, just testing if redirect works after receiving token

---
+++ spotify/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'React';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Dashboard = props => {
+    return (
+        <div>
+            <h2>Welcome to your Dashboard</h2>
+        </div>
+    )
+}
+
+export default Dashboard;
