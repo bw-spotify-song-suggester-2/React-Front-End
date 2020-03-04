d86f4442718b3d424dcdde30a787e174edf6dc42
raudelf <rawdell@outlook.com>
2020-03-04 16:55:50 -0600

    added several cases for the logging in page

--- spotify/src/reducers/index.js
+++ spotify/src/reducers/index.js
@@ -9,11 +9,29 @@ import {
 
 const initialState = {
     username: '',
-    password: ''
+    password: '',
+    loggingIn: false,
+    signingUp: false,
+    error: '',
 }
 
 const reducer = (state = initialState, action) => {
-
+        switch (action.type) {
+            case LOGIN_START:
+                return {
+                    ...state, loggingIn: true
+                };
+            case LOGIN_SUCCESS:
+                return {
+                    ...state, loggingIn: false
+                };
+            case LOGIN_ERROR:
+                return {
+                    ...state, loggingIn: false, error: 'Invalid Login'
+                };
+            default:
+                return state;
+        }
 }
 
 export default reducer;
