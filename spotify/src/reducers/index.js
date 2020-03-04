9d32d506283044a1ce97e7f7c387cdf872df5278
raudelf <rawdell@outlook.com>
2020-03-04 13:03:58 -0600

    created the index.js file for reducers

---
+++ spotify/src/reducers/index.js
@@ -0,0 +1,19 @@
+import {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_ERROR,
+    SIGNUP_START,
+    SIGNUP_SUCCESS,
+    SIGNUP_ERROR,
+} from '../actions';
+
+const initialState = {
+    username: '',
+    password: ''
+}
+
+const reducer = (state = initialState, action) => {
+
+}
+
+export default reducer;
