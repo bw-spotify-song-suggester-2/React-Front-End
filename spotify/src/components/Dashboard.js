1f2a1566b941cc0a42c187257ef4c584e37bc9e8
raudelf <rawdell@outlook.com>
2020-03-06 02:10:02 -0600

    finished laying out dashboard

--- spotify/src/components/Dashboard.js
+++ spotify/src/components/Dashboard.js
@@ -2,21 +2,60 @@ import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 // import RecommendationList from './RecommendationList';
 import { connect } from 'react-redux';
-import { fetchRecs } from '../actions';
+import { userRecs, trackRecs, clearRecs } from '../actions';
 
 const Dashboard = props => {
+    const [spotify_playlist, setTrack_Id] = useState();
+    // const [show, setShow] = useState(3)
+
+    const handleChanges = e => {
+        setTrack_Id({
+            spotify_playlist: e.target.value
+        })
+    }
     useEffect(() => {
-        props.fetchRecs();
+        props.userRecs();
     }, [])
+
+    // const handleShowMore = e => {
+    //     e.preventDefault();
+    //     setShow({
+    //         ...show, show: show >= props.similarRecs.length ? show : show + 3
+    //     })
+    // }
+
+    // const items = props.similarRecs.slice(0, show).map((item) => {
+    //     return (
+    //         <div key={item.track_id} className='musicBox'>
+    //                                     <h3>{item.track}</h3>
+    //                                     <p>{item.artist}</p>
+    //                                     <div className='trackButtons'>
+    //                                         <button onClick={()=> window.open(`https://open.spotify.com/track/${item.track_id}`, "_blank")}>Play</button>
+    //                                     </div>
+    //                                 </div>
+    //     )
+    // })
+    const link = e => {
+        props.trackRecs(spotify_playlist);
+        setTrack_Id()
+    }
+
+    const handleClear = e => {
+        e.preventDefault()
+        props.clearRecs()
+    }
+
+    console.log('Track', spotify_playlist)
     console.log('Halp?', props.recs)
-    console.log('user id: ', props.user_id)
+    console.log('user id: ', localStorage.getItem('user_id'))
     return (
         <div className='dashboard-container'>
             <h1>Welcome to your Dashboard</h1>
             {/* <RecommendationList /> */}
             <section className='user-recs'>
                 <h2>Your Recommendations</h2>
-                <div className='userRecsContainer'>
+                <button onClick={handleClear}>Clear Recommendations</button>
+                <div className='playlistRecsContainer'>
                     {props.recs.map((music, index) => {
                         return (
                             <div key={index} className='musicBox'>
@@ -31,12 +70,25 @@ const Dashboard = props => {
             <section>
                 <h2>Playlist Recommendations</h2>
                 <p>Enter a playlist URL to get started:</p>
-                <form className='playlistForm'>
-                    <input placeholder='Playlist URL'/>
+                <form className='playlistForm' onSubmit={link}>
+                    <input placeholder='Playlist URL' onChange={handleChanges}/>
+                    <button>Submit</button>
+                    <div className='playlistRecsContainer'>
+                        {props.similarRecs.map((music) => {
+                            return (
+                                    <div key={music.track_id} className='musicBox'>
+                                        <h3>{music.track}</h3>
+                                        <p>{music.artist}</p>
+                                        <div className='trackButtons'>
+                                            <button onClick={()=> window.open(`https://open.spotify.com/track/${music.track_id}`, "_blank")}>Play</button>
+                                        </div>
+                                    </div>
+                            )
+                        }) }
+                        {/* {items}
+                        <button onClick={handleShowMore}>Show More</button> */}
+                    </div>
                 </form>
-            </section>
-            <section>
-                <h2>Your Favorites</h2>
             </section>
         </div>
     )
@@ -44,7 +96,8 @@ const Dashboard = props => {
 
 const mapStateToProps = state => ({
     user_id: state.user_id,
-    recs: state.recs
+    recs: state.recs,
+    similarRecs: state.similarRecs,
 })
 
-export default connect(mapStateToProps, { fetchRecs })(Dashboard);
+export default connect(mapStateToProps, { userRecs, trackRecs, clearRecs })(Dashboard);
